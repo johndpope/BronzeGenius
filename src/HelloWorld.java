1a1c115d8c93df79a0b1c92ae4ad40fe1406fcbc
xuch <lyroechan@gmail.com>
2015-12-08 17:27:32 +0800

    new changes

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,4 +1,40 @@
+import java.io.IOException;
 import java.lang.*;
+import java.util.*;
+
+
+class Mouse {
+	private int mouseNumber;
+	Mouse(int i) {
+		mouseNumber = i;
+	}
+
+	// magic method. if not override, then will can Object.toString() by default
+	public String toString() {
+		return "This is Mouse #" + mouseNumber;
+	}
+
+	void print(String msg) {
+		if (msg != null) System.out.println(msg);
+		System.out.println("Mouse number " + mouseNumber);
+	}
+}
+
+class Dog {}
+
+class MouseTrap {
+	static void caughtYa(Object m) {
+		Mouse mouse = (Mouse)m; // cast from Objest, can detect wrong object
+		mouse.print("Caught one!");
+	}
+}
+
+class Counter {
+	int i = 1;
+	public String toString() {
+		return Integer.toString(i);
+	}
+}
 
 public class HelloWorld {
 
@@ -13,8 +49,136 @@ public class HelloWorld {
 	static void prl(String str) {
 		System.out.println(str);
 	}
+
+	static void printBitSet(BitSet b) {
+		System.out.println("bits: " + b);
+		String bbits = new String();
+		for (int i = 0; i < b.size(); i++) {
+			bbits += (b.get(i) ? "1" : "0");
+		}
+		System.out.println("bit pattern: " + bbits);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int ii = 0;
+		prl(Integer.toString(ii));
+		exit();
+
+		StrSortVector ssv = new StrSortVector();
+		ssv.addElement("d");
+		ssv.addElement("D");
+		ssv.addElement("a");
+		ssv.addElement("b");
+		ssv.addElement("t");
+		ssv.addElement("R");
+		ssv.addElement("B");
+		ssv.addElement("C");
+		ssv.addElement("c");
+		Enumeration em = ssv.elements();
+		while (em.hasMoreElements()) System.out.println(em.nextElement());
+		exit();
+
+		Properties p = System.getProperties();
+		p.list(System.out);
+		exit();
+
+		Hashtable ht = new Hashtable();
+		for (int i = 0; i < 1000000000; i++) {
+			Integer r = new Integer((int)(Math.random() * 20));
+			if (ht.containsKey(r)) ((Counter)ht.get(r)).i++;
+			else ht.put(r, new Counter());
+		}
+		System.out.println(ht);
+		exit();
+
+		AssocArray aa = new AssocArray();
+		for (char c = 'd'; c <= 'z'; c++) aa.put(String.valueOf(c), String.valueOf(c).toUpperCase());
+		char[] ca = {'d', 'e', 'i', 'o', 'u'};
+		for (int i = 0; i < ca.length; i++) System.out.println("Upper case: " + aa.get(String.valueOf(ca[i])));
+
+		exit();
+
+		Random rand = new Random();
+		byte bt = (byte)rand.nextInt();
+		BitSet bb = new BitSet();
+		for(int i = 7; i >= 0; i--) {
+			if (((1 << i) & bt) != 0) bb.set(i); // set 1 by default
+			else bb.clear(i); // set 0 by default
+		}
+		System.out.println("byte value: " + bt);
+		printBitSet(bb);
+
+		short st = (short)rand.nextInt();
+		BitSet bs = new BitSet();
+		/*for(int i = 15; i >= 0; i--) {
+			if (((1 << i) & st) != 0) bs.set(i); // set 1 by default
+			else bs.clear(i); // set 0 by default
+		}*/
+		for (int i = 0, j = 1; i < 16; i++, j <<= 1) {
+			if ((j & st) != 0) bs.set(i);
+			else bs.clear(i);
+		}
+		System.out.println("short value: " + st);
+		printBitSet(bs);
+
+		int it = rand.nextInt();
+		BitSet bi = new BitSet();
+		for (int i = 0, j = 1; i < 32; i++, j <<= 1) {
+			if ((j & it) != 0) bi.set(i);
+			else bi.clear();
+		}
+		System.out.println("int value: " + it);
+		printBitSet(bi);
+
+		BitSet b127 = new BitSet();
+		b127.set(127); // length is 128: index range 0 ~ 127
+		System.out.println("set bit 127: " + b127);
+		BitSet b255 = new BitSet(65);
+		b255.set(255);
+		System.out.println("set bit 255: " + b255);
+		BitSet b1023 = new BitSet(512);
+		//b1023.set(1023);
+		b1023.set(1024); // out of the range of 512
+		System.out.println("set bit 1023: " + b1023);
+
+		exit();
+
+
+
+
+
+
+
+		Vector mice = new Vector();
+		for (int i =0; i < 3; i++) {
+			mice.addElement(new Mouse(i));
+		}
+		//mice.addElement(new Dog());
+
+		for (int i = 0; i < mice.size(); i++) {
+			System.out.println("Free mouse: " + mice.elementAt(i));
+			MouseTrap.caughtYa(mice.elementAt(i)); // detect the wrong class, and exception thrown here when running
+		}
+
+		Enumeration en = mice.elements();
+		while (en.hasMoreElements()) {
+			System.out.println(en.nextElement().toString());
+		}
+		exit();
+
+
+
+		try {
+			String current = new java.io.File(".").getCanonicalPath();
+			System.out.println("Current dir:" + current);
+			String currentDir = System.getProperty("user.dir");
+			System.out.println("Current dir using System:" + currentDir);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		exit();
+
 		String str = "1|2|3||4|5|6";
 		String sa[] = str.split("\\||");
 
@@ -82,3 +246,122 @@ public class HelloWorld {
 	}
 
 }
+
+
+class AssocArray extends Dictionary
+{
+	private Vector keys = new Vector();
+	private Vector values = new Vector();
+
+	public int size() {return keys.size();}
+	public boolean isEmpty() {return keys.isEmpty();}
+
+	public Object put(Object key, Object value) {
+		keys.addElement(key);
+		values.addElement(value);
+		return key;
+	}
+
+	public Object get(Object key) {
+		int index = keys.indexOf(key);
+		if (index == -1) return null;
+		return values.elementAt(index);
+	}
+
+	public Object remove(Object key) {
+		int index = keys.indexOf(key);
+		if (index == -1) return null;
+		keys.removeElementAt(index);
+		Object rt = values.elementAt(index);
+		values.removeElementAt(index);
+		return rt;
+	}
+
+	public Enumeration keys() {return keys.elements();}
+
+	public Enumeration elements() {return values.elements();}
+
+}
+
+interface Compare
+{
+	boolean lessThan(Object l, Object r);
+	boolean lessThanOrEqual(Object l, Object r);
+}
+
+class SortVector extends Vector {
+	private Compare compare;
+
+	public SortVector(Compare comp) {
+		compare = comp;
+	}
+
+	public void sort() {
+		quickSort(0, size() - 1);
+	}
+
+	private void quickSort(int left, int right) {
+		if (right > left) {
+			Object ol = elementAt(right);
+			int i = left - 1;
+			int j = right;
+			while (true) {
+				while (compare.lessThan(elementAt(++i), ol));
+				while (j > 0) if (compare.lessThanOrEqual(elementAt(--j), ol)) break;
+				if (i >= j) break;
+				swap(i, j);
+			}
+			swap(i, right);
+			quickSort(left, i - 1);
+			quickSort(i + 1, right);
+		}
+	}
+
+	private void swap(int l, int r) {
+		Object tmp = elementAt(l);
+		setElementAt(elementAt(r), l);
+		setElementAt(tmp, r);
+	}
+}
+
+class StrSortVector {
+	private SortVector v = new SortVector(
+		// anonymous inner class: 匿名内部类，没有具体的名字，可以保证只在内部使用
+		new Compare() {
+			public boolean lessThan(Object l, Object r) {
+				return ((String)l).toLowerCase().compareTo(((String)r).toLowerCase()) < 0;
+			}
+
+			public boolean lessThanOrEqual(Object l, Object r) {
+				return ((String)l).toLowerCase().compareTo(((String)r).toLowerCase()) <= 0;
+			}
+		}
+	);
+
+	private boolean sorted = false;
+	public void addElement(String s) {
+		v.addElement(s);
+		sorted = false;
+	}
+
+	public String elementAt(int index) {
+		if (!sorted) {
+			v.sort();
+			sorted = true;
+		}
+		return (String)v.elementAt(index);
+	}
+
+	public Enumeration elements() {
+		if (!sorted) {
+			v.sort();
+			sorted = true;
+		}
+		return v.elements();
+	}
+}
+
+
+
+// https://w.amazon.com/index.php/Linux/Desktop/DNS
+// https://w.amazon.com/index.php/Linux/Desktop/Single_user_mode
---
+++ src/Mail/Email.java
@@ -0,0 +1,142 @@
+package Mail;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.util.Properties;
+import java.io.UnsupportedEncodingException;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.activation.*;
+
+
+/**
+ * Created by xuch on 2015/12/7.
+ */
+public class Email {
+    public static void main(String[] args) {
+
+        String  d_email = "lyroechan@gmail.com",
+                d_uname = "Name",
+                d_password = "9887765432lyroechan",//"AIzaSyCeh2KTi99bSAyOaHu-TzZ9t0ChVHwjOUk",//"9887765432lyroechan",
+                d_host = "smtp.gmail.com",
+                d_port  = "465",
+                m_to = "xuch@amazon.com",
+                m_subject = "This is from JavaMail Test! ",// + params[0].getName(),
+                m_text = "This message is from Indoor Positioning App. Required file(s) are attached.";
+        Properties props = new Properties();
+        props.put("mail.smtp.user", d_email);
+        props.put("mail.smtp.host", d_host);
+        props.put("mail.smtp.port", d_port);
+        props.put("mail.smtp.starttls.enable","true");
+        props.put("mail.smtp.debug", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.socketFactory.port", d_port);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.socketFactory.fallback", "false");
+
+        //SMTPAuthenticator auth = new SMTPAuthenticator();
+        //Session session = Session.getInstance(props, auth);
+        Session session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                //return super.getPasswordAuthentication();
+                return new PasswordAuthentication(d_email, d_password);
+            }
+        });
+        session.setDebug(true);
+
+        MimeMessage msg = new MimeMessage(session);
+        try {
+            msg.setSubject(m_subject);
+            msg.setFrom(new InternetAddress(d_email));
+            msg.addRecipient(Message.RecipientType.TO, new InternetAddress(m_to));
+            //msg.addRecipient(Message.RecipientType.TO, InternetAddress.parse(m_to));
+            msg.addRecipients(Message.RecipientType.TO, new InternetAddress[]{new InternetAddress("lyroechan@gmail.com"), new InternetAddress(m_to),});
+            //msg.setText(m_text);
+
+            // set the body text
+            MimeBodyPart mimeBodyPart = new MimeBodyPart();
+            mimeBodyPart.setContent("<h1> <font color = red>" + m_text + "</h1> </font>", "text/html");
+            //mimeBodyPart.setText(m_text);
+            //mimeBodyPart.setHeader("Content-Type", "test/html; charset= \"UTF-8\"");
+
+            Multipart multipart = new MimeMultipart();
+            multipart.addBodyPart(mimeBodyPart);
+
+            // add the attachments
+            String fileDir = new java.io.File(".").getCanonicalPath() + "\\src\\Mail\\", fileName = "EmailA.java";
+            mimeBodyPart = new MimeBodyPart();
+            DataSource source = new FileDataSource(fileDir + fileName);
+            mimeBodyPart.setDataHandler(new DataHandler(source));
+            mimeBodyPart.setFileName(fileName);
+            String contentType = Files.probeContentType(FileSystems.getDefault().getPath(fileDir, fileName));
+            mimeBodyPart.setHeader("Content-Type", contentType + "; name =\"" + fileName + "\"");
+            mimeBodyPart.setHeader("Content-Transfer-Encoding", "base64");
+
+            multipart.addBodyPart(mimeBodyPart);
+
+            msg.setContent(multipart);
+
+            Transport transport = session.getTransport("smtps");
+            transport.connect(d_host, Integer.valueOf(d_port), d_uname, d_password);
+            transport.sendMessage(msg, msg.getAllRecipients());
+            transport.close();
+
+        } catch (AddressException e) {
+            e.printStackTrace();
+            System.exit(-1);//return false;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            System.exit(-1);//return false;
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+
+
+        /*final String username = "lyroechan@gmail.com";
+        final String password = "9887765432lyroechan";
+        final String to = "xuch@amazon.com";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "25");//587 465
+
+        Session session = Session.getInstance(props,
+                new javax.mail.Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(username));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(to));
+            message.setSubject("Testing Subject");
+            message.setText("Dear Mail Crawler,"
+                    + "\n\n No spam to my email, please!");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }*/
+    }
+}
+
+
+/*
+    OAuth 用戶端
+    這是您的用戶端 ID:
+    319694605841-9jh8taudo5kg5ranoc2t2i1m6rec35aj.apps.googleusercontent.com
+    您的用戶端密鑰如下:
+    _wpBfsQenQmVZmlBJHss_L-x
+ */
